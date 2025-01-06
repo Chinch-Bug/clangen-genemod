ab8a3d805c9b88acc93ac4f25b9661511ad8dc64
Chinch-Bug <77509218+Chinch-Bug@users.noreply.github.com>
2025-01-06 14:05:55 +0200

    fixing the prev names not being saved

--- scripts/cat/cats.py
+++ scripts/cat/cats.py
@@ -1399,6 +1399,7 @@ class Cat:
 
         if not os.path.exists(cat_history_directory):
             self.history = History(
+                prev_names=[],
                 beginning={},
                 mentor_influence={},
                 app_ceremony={},
@@ -1413,6 +1414,9 @@ class Cat:
             with open(cat_history_directory, "r", encoding="utf-8") as read_file:
                 history_data = ujson.loads(read_file.read())
                 self.history = History(
+                    prev_names=(
+                        history_data["prev_names"] if "prev_names" in history_data else []
+                    ),
                     beginning=(
                         history_data["beginning"] if "beginning" in history_data else {}
                     ),
@@ -1468,6 +1472,7 @@ class Cat:
             game.safe_save(f"{history_dir}/{self.ID}_history.json", history_dict)
         except:
             self.history = History(
+                prev_names=[],
                 beginning={},
                 mentor_influence={},
                 app_ceremony={},
