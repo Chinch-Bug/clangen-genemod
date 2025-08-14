9b1f760ce97ab3f5a3a2652e694694f7e08b4d80
Chinch-Bug <77509218+Chinch-Bug@users.noreply.github.com>
2025-08-14 11:03:36 +0300

    fixing more missing grief files

--- scripts/cat/cats.py
+++ scripts/cat/cats.py
@@ -903,10 +903,10 @@ class Cat:
                 rel_type = [k for k in rel_type_tiers if tier in rel_type_tiers[k]]
                 if tier.is_extreme_pos:
                     very_high_types.extend(rel_type)
-                elif tier.is_mid_pos:
-                    list_to_extend = choice([very_low_types, very_high_types])
-                    list_to_extend.extend(rel_type)
                 elif tier.is_low_pos:
+                    list_to_extend = choice([very_low_types, high_types, very_high_types])
+                    list_to_extend.extend(rel_type)
+                elif tier.is_mid_pos:
                     high_types.extend(rel_type)
                 elif tier.is_extreme_neg or tier.is_mid_neg:
                     very_low_types.extend(rel_type)
