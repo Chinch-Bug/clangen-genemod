e81cd522efacdf3cba75cea0a01842a7c13fb757
Chinch-Bug <77509218+Chinch-Bug@users.noreply.github.com>
2026-02-01 13:16:31 +0200

    bugfix

--- scripts/events_module/short/short_event_generation.py
+++ scripts/events_module/short/short_event_generation.py
@@ -284,7 +284,7 @@ def generate_event_objects(event_triggered, biome, frequency) -> list:
                     ),
                     m_c=event["m_c"] if "m_c" in event else {},
                     r_c=event["r_c"] if "r_c" in event else {},
-                    new_cat=event["new_cat"] if "new_cat" in event else [],
+                    new_cat=event["multiclan_cat"] if game.clan.clancount == "multiclan" and "multiclan_cat" in event else (event["new_cat"] if "new_cat" in event else []),
                     injury=event["injury"] if "injury" in event else [],
                     exclude_involved=(
                         event["exclude_involved"] if "exclude_involved" in event else []
