94bd56094597e74ef62e8784702baac5bb62fc58
Chinch-Bug <77509218+Chinch-Bug@users.noreply.github.com>
2026-07-06 22:52:08 +0300

    pylint

--- tests/test_cat.py
+++ tests/test_cat.py
@@ -8,7 +8,8 @@ import i18n
 os.environ["SDL_VIDEODRIVER"] = "dummy"
 os.environ["SDL_AUDIODRIVER"] = "dummy"
 
-from scripts.game_structure import game, constants
+from scripts.game_structure import game
+from scripts.config import CONFIG
 
 from scripts.cat.cats import Cat
 from scripts.cat_relations.inheritance2 import inheritance_db
@@ -452,7 +453,7 @@ class TestNameRepr(unittest.TestCase):
     def setUpClass(cls):
         os.environ["SDL_VIDEODRIVER"] = "dummy"
         os.environ["SDL_AUDIODRIVER"] = "dummy"
-        constants.CONFIG["cat_name_controls"]["always_use_english"] = True
+        CONFIG["cat_name_controls"]["always_use_english"] = True
 
     def test_clancats(self):
         """
