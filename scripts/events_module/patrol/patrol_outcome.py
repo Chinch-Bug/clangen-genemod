6b507c6238f014f4d8ee9bd6bf234364795e253a
Chinch-Bug <77509218+Chinch-Bug@users.noreply.github.com>
2026-08-08 03:17:15 +0300

    another one

--- scripts/events_module/patrol/patrol_outcome.py
+++ scripts/events_module/patrol/patrol_outcome.py
@@ -789,8 +789,7 @@ class PatrolOutcome:
                 quantity_allowed += get_config("clan_resources.herbs.general_patrol_quantity_per_cat")
 
                 skills = cat.skills.get_all()
-                amount_modifier += skills.get(SkillPath.SENSE, 0)
-                quantity_modifier += skills.get(SkillPath.CLEVER, 0)
+                quantity_allowed += skills.get(SkillPath.CLEVER, 0)
             # get random herbs, add to storage, and get patrol outcome msg
             list_of_herb_strs, found_herbs = game.clan.herb_supply.get_found_herbs(
                 med_cat=patrol.patrol_leader,
