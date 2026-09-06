ff2b0aa9e133e851d721b7e2fd2d1ec81e02837d
Chinch-Bug <77509218+Chinch-Bug@users.noreply.github.com>
2026-09-06 17:36:08 +0300

    no longer counted as former mentor/apprentice if mentor is changed at 6 moons old

--- scripts/cat/cats.py
+++ scripts/cat/cats.py
@@ -1756,10 +1756,11 @@ class Cat:
         if self.ID in mentor_cat.apprentice:
             mentor_cat.apprentice.remove(self.ID)
 
-        if self.ID not in mentor_cat.former_apprentices:
-            mentor_cat.former_apprentices.append(self.ID)
-        if mentor_cat.ID not in self.former_mentor:
-            self.former_mentor.append(mentor_cat.ID)
+        if self.moons > self.age_moons[CatAge.ADOLESCENT][0]:
+            if self.ID not in mentor_cat.former_apprentices:
+                mentor_cat.former_apprentices.append(self.ID)
+            if mentor_cat.ID not in self.former_mentor:
+                self.former_mentor.append(mentor_cat.ID)
         self.mentor = None
 
     def __add_mentor(self, new_mentor_id: str):
